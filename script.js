1e3f41f17b2c750af9c9f183edfa915bbe571ebe
regularwave <regularwave@gmail.com>
2024-04-23 20:22:12 -0400

    removed nonfunctional counter

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 const pack = document.getElementById('pack');
-const visCount = document.getElementById('visCount');
-updateSiteCounter();
 
 function openPack() {
     var img = document.createElement('img');
     img.src = "./img/moneyfire.gif";
     pack.replaceChildren(img);
-}
-
-function updateSiteCounter() {
-    fetch('https://api.countapi.xyz/hit/regularwave_mtg-30AE-sim')
-        .then(response => response.json())
-        .then(response => {
-            visCount.innerHTML = response.value;
-        })
 }
